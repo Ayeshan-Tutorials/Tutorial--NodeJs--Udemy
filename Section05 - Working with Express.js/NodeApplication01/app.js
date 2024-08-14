a310881818de28a6a50ccf077700ffcf3ba1bb3a
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-14 18:48:56 +0530

    End of 61 (5) - Adding Middleware

--- Section05 - Working with Express.js/NodeApplication01/app.js
+++ Section05 - Working with Express.js/NodeApplication01/app.js
@@ -4,6 +4,15 @@ const express = require('express')
 
 const app=express();
 
+app.use((req, res, next) => {
+    console.log("In the  middlewear!");
+    next(); // allowa the request to continue to the next middlewear in line
+})
+
+app.use((req, res, next) => {
+    console.log("In the other  middlewear!")
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
